ef14e0a9ef65651897b2abfdaa05e943e5cf58bc
Matsuri <2440460202@qq.com>
2022-07-22 03:00:43 +0800

    init ssm_proj01

---
+++ ssm_proj01/src/com/matsuri/test/Test.java
@@ -0,0 +1,11 @@
+package com.matsuri.test;
+
+/**
+ * Created by Matsuri on 2022/7/22 - 2:59
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Testing this proj");
+    }
+}
